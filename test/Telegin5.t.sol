8c5f7818389e61968faa94a85dab210269c1c8f8
Alex Telegin <telegin.alexander@gmail.com>
2024-09-15 19:40:11 +0300

    Add unchecked arithmetics to T5

--- test/Telegin5.t.sol
+++ test/Telegin5.t.sol
@@ -14,49 +14,49 @@ contract Telegin5Test is Test {
     }
 
     function test() public view {
-        (int256 a) = telegin.solution5(-3, 1);
+        (int256 a) = telegin.solution(-3, 1);
         assertEq(a, -1, "Incorrect"); // -1 + 0
-        (int256 b) = telegin.solution5(-4, 2);
+        (int256 b) = telegin.solution(-4, 2);
         assertEq(b, -1, "Incorrect"); // -2 + 1 
-        (int256 c) = telegin.solution5(-4, 1);
+        (int256 c) = telegin.solution(-4, 1);
         assertEq(c, -1, "Incorrect"); /// -2 = -2 + 0 !!!!!!!
-        (int256 c0001) = telegin.solution5(-4, 7);
+        (int256 c0001) = telegin.solution(-4, 7);
         assertEq(c0001, 2, "Incorrect");
-        (int256 c000) = telegin.solution5(1, 7);
+        (int256 c000) = telegin.solution(1, 7);
         assertEq(c000, 4, "Incorrect");
-        (int256 cd) = telegin.solution5(-3, 2);
+        (int256 cd) = telegin.solution(-3, 2);
         assertEq(cd, 0, "Incorrect");
-        (int256 cdd) = telegin.solution5(-1, 4);
+        (int256 cdd) = telegin.solution(-1, 4);
         assertEq(cdd, 2, "Incorrect");
-        (int256 cddd) = telegin.solution5(-1, 5);
+        (int256 cddd) = telegin.solution(-1, 5);
         assertEq(cddd, 2, "Incorrect");
-        (int256 cddd1) = telegin.solution5(-3, 5);
+        (int256 cddd1) = telegin.solution(-3, 5);
         assertEq(cddd1, 1, "Incorrect");
-        (int256 cddd2) = telegin.solution5(-3, 6);
+        (int256 cddd2) = telegin.solution(-3, 6);
         assertEq(cddd2, 2, "Incorrect");
-        (int256 cddd22) = telegin.solution5(-8, 3);
+        (int256 cddd22) = telegin.solution(-8, 3);
         assertEq(cddd22, -2, "Incorrect"); // - 4 + 1 = -3!!!!!!
-        (int256 cddd229) = telegin.solution5(-7, 2);
+        (int256 cddd229) = telegin.solution(-7, 2);
         assertEq(cddd229, -2, "Incorrect");
-        (int256 cddd23) = telegin.solution5(-8, 4);
+        (int256 cddd23) = telegin.solution(-8, 4);
         assertEq(cddd23, -2, "Incorrect");
-        (int256 ccc) = telegin.solution5(-1, 4);
+        (int256 ccc) = telegin.solution(-1, 4);
         assertEq(ccc, 2, "Incorrect"); /// 2 = 0 + 2
-        (int256 cc) = telegin.solution5(-3, 3);
+        (int256 cc) = telegin.solution(-3, 3);
         assertEq(cc, 0, "Incorrect");
-        (int256 d) = telegin.solution5(-3, 2);
+        (int256 d) = telegin.solution(-3, 2);
         assertEq(d, 0, "Incorrect"); // -1 + 1
-        (int256 e) = telegin.solution5(2, 4);
+        (int256 e) = telegin.solution(2, 4);
         assertEq(e, 3, "Incorrect"); // 1 + 2 
-        (int256 f) = telegin.solution5(2, 5);
+        (int256 f) = telegin.solution(2, 5);
         assertEq(f, 4, "Incorrect"); ///// 3 = 1 + 2 !!!!!!
-        (int256 i) = telegin.solution5(-10, -6);
+        (int256 i) = telegin.solution(-10, -6);
         assertEq(i, -8, "Incorrect"); // -5 + -3
-        (int256 j) = telegin.solution5(-10, -5);
+        (int256 j) = telegin.solution(-10, -5);
         assertEq(j, -7, "Incorrect"); // -5 + -2 
-        (int256 jj) = telegin.solution5(-9, -5);
+        (int256 jj) = telegin.solution(-9, -5);
         assertEq(jj, -7, "Incorrect"); // -6 = -4 + -2 !!!!!!
-        (int256 jjj) = telegin.solution5(9, 5);
+        (int256 jjj) = telegin.solution(9, 5);
         assertEq(jjj, 7, "Incorrect"); // 6 = 4 + 2 !!!!!!
         // оба положительные 
         // положительный и отрицательный
