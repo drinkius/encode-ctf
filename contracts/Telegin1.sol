b9737a658ae9e84d14658aae07d26e436a8637c4
Alex Telegin <telegin.alexander@gmail.com>
2024-09-15 17:00:27 +0300

    Renturn previous T1 since it's lower gas

--- contracts/Telegin1.sol
+++ contracts/Telegin1.sol
@@ -8,6 +8,20 @@ contract Telegin1 {
     function solution(
       uint256[2][3] calldata x, 
       uint256[2][3] calldata y) external pure returns (uint256[2][3] memory result) {
+        // 139258 gas 
+        unchecked {
+          finalArray[0][0] = x[0][0] + y[0][0];
+          finalArray[1][0] = x[1][0] + y[1][0];
+          finalArray[2][0] = x[2][0] + y[2][0];
+          
+          finalArray[0][1] = x[0][1] + y[0][1];
+          finalArray[1][1] = x[1][1] + y[1][1];
+          finalArray[2][1] = x[2][1] + y[2][1];
+        }
+        return finalArray;
+
+      // this in unfortunately worse - 154300
+      /*
         assembly {
            let xPtr := x
            let yPtr := y
@@ -26,5 +40,6 @@ contract Telegin1 {
           // 0x100 - [2, 1]
           mstore(add(resultPtr, 0x100), add(calldataload(add(xPtr, 0xA0)), calldataload(add(yPtr, 0xA0))))
        }
+       */
     }
 }
